4e2b77d358e0a50e49ef6b76ca8ef50479b73dbc
橋本准 <hashimotojun@jun.local>
2020-06-26 02:24:57 +0900

    commit

--- src/components/Label.vue
+++ src/components/Label.vue
@@ -1,15 +1,8 @@
 <template>
   <div class="label">
-    <label
-      v-for="(item, i) in list"
-      :key="i"
-      @change="searchapi()"
-      id="labelWrap"
-    >
+    <label v-for="(item, i) in list" :key="i" @change="searchapi()" id="labelWrap">
       <input v-model="dogname" :value="i" type="radio" />
-      <div class="wrap">
-        {{ i }}
-      </div>
+      <div class="wrap">{{ i }}</div>
     </label>
   </div>
 </template>
@@ -39,26 +32,26 @@ export default {
 
 <style lang="scss" scoped>
 input[type="radio"] {
-  display: none; /* ラジオボタンを非表示にする */
+  display: none;
 }
 .wrap {
-  display: block; /* ブロックレベル要素化する */
-  float: left; /* 要素の左寄せ・回り込を指定する */
-  margin: 5px; /* ボックス外側の余白を指定する */
-  width: 150px; /* ボックスの横幅を指定する */
-  height: 45px; /* ボックスの高さを指定する */
-  padding-left: 5px; /* ボックス内左側の余白を指定する */
-  padding-right: 5px; /* ボックス内御右側の余白を指定する */
-  color: #b20000; /* フォントの色を指定 */
-  text-align: center; /* テキストのセンタリングを指定する */
-  line-height: 45px; /* 行の高さを指定する */
-  cursor: pointer; /* マウスカーソルの形（リンクカーソル）を指定する */
-  border: 2px solid #006dd9; /* ボックスの境界線を実線で指定する */
-  border-radius: 5px; /* 角丸を指定する */
+  display: block;
+  float: left;
+  margin: 5px;
+  width: 150px;
+  height: 45px;
+  padding-left: 5px;
+  padding-right: 5px;
+  color: #b20000;
+  text-align: center;
+  line-height: 45px;
+  cursor: pointer;
+  border: 2px solid #006dd9;
+  border-radius: 5px;
 }
 input[type="radio"]:checked + .wrap {
-  background: #31a9ee; /* マウス選択時の背景色を指定する */
-  color: #ffffff; /* マウス選択時のフォント色を指定する */
+  background: #31a9ee;
+  color: #ffffff;
 }
 .clickClass {
   background: #006dd9;
